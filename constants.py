f80d7faedfe5d237fb4845c548a64b4fd4cb6bac
Mohammad Ahmadi <155301965+mahmadii0@users.noreply.github.com>
2025-02-19 11:11:07 +0330

    Create constants.py for Github

---
+++ constants.py
@@ -0,0 +1,22 @@
+Token= "Your telegram bot token"
+
+#database variables
+connectionDetail={
+    'host':'',
+    'port':'',
+    'user':'',
+    'password':'',
+    'database':'',
+    }
+
+BotUserIds=[]
+
+Rolelist=['Godfather','Matador','Sual Goodman','Simple Citizen','Simple Citizen'
+    ,'Simple Citizen','Leon','Doctor','Constantine','Kein','Sherlock','پدرخوانده','ماتادور','ساول گودمن','شهروند ساده','شهروند ساده'
+    ,'شهروند ساده','لئون','دکتر','کنستانتین','شهروند کین','شرلوک']
+
+Rolelist_en=['Godfather','Matador','Sual Goodman','Simple Citizen','Simple Citizen'
+    ,'Simple Citizen','Leon','Doctor','Constantine','Kein','Sherlock']
+
+Rolelist_fa=['پدرخوانده','ماتادور','ساول گودمن','شهروند ساده','شهروند ساده'
+    ,'شهروند ساده','لئون','دکتر','کنستانتین','شهروند کین','شرلوک']
